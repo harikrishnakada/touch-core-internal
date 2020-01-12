c0d4cd4046b86e42969114582079676602a504f0
Hari Krishna <Harik@touchcoresystems.com>
2020-01-13 00:19:39 +0530

    Added employee list data table

--- touch-core-internal/ClientApp/src/app/employee-list/employee-list.component.ts
+++ touch-core-internal/ClientApp/src/app/employee-list/employee-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataTablesModule } from 'angular-datatables';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-employee-list',
@@ -8,40 +9,40 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
-
+  //@ViewChild(DataTablesModule)
   employees: any = [];
-  dtOptions: DataTables.Settings = {};
+  dtElement: DataTablesModule;
+  dtOptions: {};
+  dtTrigger: Subject<any> = new Subject<any>();
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get('https://9253ccf3-458a-4b37-8c2e-9a5b8b07d344.mock.pstmn.io/employees').subscribe((resp: Response) => {
+    this.http.get('https://c6b60f2b-52d4-4233-88ea-6c1ed0158c18.mock.pstmn.io/Employees').subscribe((resp: Response) => {
       this.employees = resp;
+      // Calling the DT trigger to manually render the table
+      this.dtTrigger.next();
      });
    
      this.dtOptions = {
-      columnDefs: [
-        { defaultContent: "", targets: "_all" },
-        { targets: [1, 2, 3, 4, 5, 6], orderable: true },
-        { targets: "_all", orderable: false }
-      ],
-      language: {
-        info: "Items _START_ to _END_ of _TOTAL_",
-        lengthMenu: "Page Size:  _MENU_",
-        processing: "",
-        zeroRecords: "No data available"
-    },
-    dom: "<'row'<'col-sm-3'B>>" + "<'row'<'col-sm-12'tr>>" +
-        "<'row table-control-row'<'col-sm-3'i><'col-sm-3'l><'col-sm-6'p>>",
-    processing: false,
-    info: true,
-    paging: true,
-    searching: false,
-    pageLength: 1,
-    lengthChange: false,
-    autoWidth: false,
-    destroy: true,
-      // Configure the buttons
-    }
+      pagingType: 'full_numbers',
+      // columnDefs: [
+      //   {
+      // }
+      // ],
+      lengthMenu: [5, 20, 40],
+      pageLength: 5,
+      dom: 'Bfrtip',
+      // dom: "<'row'<'col-sm-3'B>>" + "<'row'<'col-sm-12'tr>>" +
+      // "<'row table-control-row'<'col-sm-3'i><'col-sm-3'l><'col-sm-6'p>>",
+      // buttons: [
+      //  'copy',
+      //  'print',
+      //  'excel',
+      // ]
+      "buttons": [
+        { "extend": 'print', "text":'Print',"className": 'fa fa-print btn btn-info btn-md' },
+    ],
+    };
   }
 }
