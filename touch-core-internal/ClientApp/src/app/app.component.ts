3040089ad3c230ffbc014ac21015d4282de49ea6
Hari Krishna <Harik@touchcoresystems.com>
2020-01-16 00:26:02 +0530

    Added authorization to the application.

--- touch-core-internal/ClientApp/src/app/app.component.ts
+++ touch-core-internal/ClientApp/src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+    title = 'app';
+
+    constructor(private authService: AuthService) {}
+
+    ngOnInit() {
+        console.log("App Component is rendered");
+        console.log(sessionStorage.getItem('accessToken'));
+
+       // this.authService.getUser();
+      
+    }
 }
--- touch-core-internal/ClientApp/src/app/app.module.ts
+++ touch-core-internal/ClientApp/src/app/app.module.ts
@@ -15,34 +15,53 @@ import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeFormComponent } from './employee-form/employee-form.component';
 import { BadgesComponent } from './badges/badges.component';
 import { RewardsListComponent } from './rewards-list/rewards-list.component';
+import { AuthService } from './services/auth.service';
+import { AppSettings } from './app.config';
+
+import { MsalModule, MsalGuard } from '@azure/msal-angular';
+import { AuthorizeDirective } from './directives/authorize.directive';
+
+//import { Observable } from 'rxjs/Observable';
+
+export const protectedResourceMap: [string, string[]][] = [['https://graph.microsoft.com/v1.0/me', ["user.read"]]];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
-    EmployeeListComponent,
-    EmployeeFormComponent,
-    BadgesComponent,
-    RewardsListComponent
-  ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    FormsModule,
-    CommonModule,
-    DataTablesModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'employee', component: EmployeeListComponent },
-      { path: 'reward', component: RewardsListComponent },
-    ])
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        HomeComponent,
+        CounterComponent,
+        FetchDataComponent,
+        EmployeeListComponent,
+        EmployeeFormComponent,
+        BadgesComponent,
+        RewardsListComponent,
+        AuthorizeDirective
+    ],
+    imports: [
+        BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+        HttpClientModule,
+        FormsModule,
+        CommonModule,
+        DataTablesModule,
+        MsalModule.forRoot({
+            clientID: AppSettings.applicationId,
+            authority: `${AppSettings.authority}/${AppSettings.tenantId}`,
+            consentScopes: AppSettings.consentScopes,
+            redirectUri: AppSettings.redirectUri,
+            postLogoutRedirectUri: AppSettings.postLogoutRedirectUri,
+            cacheLocation: AppSettings.cacheLocation,
+            protectedResourceMap: protectedResourceMap
+        }),
+        RouterModule.forRoot([
+            { path: '', component: HomeComponent, pathMatch: 'full' },
+            { path: 'counter', component: CounterComponent },
+            { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'employee', component: EmployeeListComponent },
+            { path: 'reward', component: RewardsListComponent },
+        ])
+    ],
+    providers: [AuthService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
